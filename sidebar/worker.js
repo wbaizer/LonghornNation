6cd3fab2412d6aedb02673dbe3c4becfb68740c9
lazymondaysunday <lazymondaysunday@gmail.com>
2019-02-15 17:33:03 -0600

    added discord bot to worker

--- sidebar/worker.js
+++ sidebar/worker.js
@@ -13,6 +13,7 @@ const { message, client } = require('./utils/discord');
 const { buildTitle } = require('./utils/gameThread');
 const teamLink = require('./static_data/teams.reddit.json');
 const networks = require('./static_data/networks.json');
+const messageHandler = require('./utils/discord_utils/messageHandler');
 let agenda = require('./jobs/agenda');
 
 
@@ -21,6 +22,11 @@ console.log('Starting Up App');
 agenda.on('ready', function() {
     agenda.create('Update Sidebar').unique({'subreddit': process.env.SUBREDDIT}).repeatEvery('0 0 * * *', {skipImmediate: false}).save();
     agenda.create('Free Talk Thread').unique({'ftt-sub': process.env.SUBREDDIT}).repeatEvery('0 0 * * *', {skipImmediate: true}).save();
+});
+
+
+client.on('ready', () => {
+    console.log('Discord Ready');
 });
 
 console.log('created recurring event');
