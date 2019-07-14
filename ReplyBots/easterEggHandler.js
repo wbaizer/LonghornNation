c87042787a808670e0be2f5e80bc2565eea8896e
Brian Hoang <>
2019-07-13 22:53:56 -0700

    giles handler

--- ReplyBots/easterEggHandler.js
+++ ReplyBots/easterEggHandler.js
@@ -1,7 +1,7 @@
 const bruHandler = require('./bruHandler');
 const fireTomHermanHandler = require('./fireTomHermanHandler');
 const fireShakaHandler = require('./fireShakaHandler');
-
+const gilesHandler = require('./gilesHandler');
 
 const easterEggHandler = (message, reply) => {
   const tokens = message.trim().split(
@@ -10,6 +10,7 @@ const easterEggHandler = (message, reply) => {
   bruHandler(tokens, reply);
   fireTomHermanHandler(message, reply);
   fireShakaHandler(message, reply);
+  gilesHandler(message, reply);
 }
 
 module.exports = easterEggHandler;
---
+++ ReplyBots/gilesHandler.js
@@ -0,0 +1,8 @@
+
+const gilesHandler = (message, reply) => {
+  if (message.toLowerCase().includes('giles')) {
+    reply('https://media.discordapp.net/attachments/289481914384121856/599360340144160778/gilin.gif');
+  }
+}
+
+module.exports = gilesHandler;
