acb6586026e4fac4435e515201e463a488d66e02
Brian Hoang <>
2019-08-02 18:35:25 -0500

    remove unnecessary reddit stuff in CB bot

--- CrystalBallBot/index.js
+++ CrystalBallBot/index.js
@@ -1,22 +1,13 @@
 const Discord = require('discord.js');
 const Twitter = require('twitter');
-const snoowrap = require('snoowrap');
-
-const reddit = new snoowrap({
-    userAgent: '/r/LHN BatmanBot',
-    clientId: process.env.REDDIT_KEY,
-    clientSecret: process.env.REDDIT_SECRET,
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-  });
 
 const discord = new Discord.Client();
 
 const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 const params = {
@@ -31,16 +22,20 @@ const discordSendMessage = (channel, message) => {
 }
 
 discord.on('ready', () => {
-  var stream = client.stream('statuses/filter', params, (stream) => {
-    stream.on('data', function(event) {
-      if (event != null) {
-        const url = `https://twitter.com/${event.user.screen_name}/status/${event.id_str}`;
-        var chan = discord.channels.get("601575968804700160");
-        chan.send(url);
-      }
+  try {
+    var stream = client.stream('statuses/filter', params, (stream) => {
+      stream.on('data', function(event) {
+        if (event != null) {
+          const url =
+            `https://twitter.com/${event.user.screen_name}/status/${event.id_str}`;
+          var chan = discord.channels.get('601575968804700160');
+          chan.send(url);
+        }
+      });
     });
-  });
+  } catch (e) {
+
+  }
 });
 
 discord.login(process.env.BOT_TOKEN);
-
