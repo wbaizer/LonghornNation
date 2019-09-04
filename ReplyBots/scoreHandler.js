117ccb7c3c8782f51cdcd9d51279fa14bd815a76
Brian Hoang <>
2019-09-04 00:55:01 -0700

    add scoreboard

--- ReplyBots/scoreHandler.js
+++ ReplyBots/scoreHandler.js
@@ -18,52 +18,103 @@ const ordinalConverter = (num) => {
   }
 }
 
-const scoreHandler = (send) => {
-  const url = 'https://site.web.api.espn.com/apis/site/v2/sports/football/college-football/teams/251/schedule?region=us&lang=en&seasontype=2&enable=broadcasts&disable=leaders';
+const spacify = (s, width) => {
+  let ret = s;
+  for(i = 0; i < width - s.length; i++) {
+    ret += " ";
+  }
+  return ret;
+}
+
+const repeat = (c, width) => {
+  let ret = "";
+  for (i = 0; i < width; i++) {
+    ret += c;
+  }
+  return ret;
+}
+
+const prettifyScoreboard = (scoreboard) => {
+  const width = 9;
+  const competition = scoreboard.competitions[0];
+  const firstTeam = competition.competitors[0].team.abbreviation;
+  const secondTeam =  competition.competitors[1].team.abbreviation;
+  const status = competition.status.type.name;
+  const team1Rank = competition.competitors[0].curatedRank.current;
+  const team2Rank = competition.competitors[1].curatedRank.current;
   
-  const test = axios.get(url).then(response => {
-    const events = response.data.events;
-    
-    const currentTime = moment();
-    
-    const gameEvent = events.find(
-      e => {
-        const eventMoment = moment(e.date);
-        return currentTime.diff(eventMoment, 'days') < 3;
+  let team1DisplayRank = team1Rank > 25 ? "   " : spacify(`#${team1Rank}`, 3);
+  let team2DisplayRank = team2Rank > 25 ? "   " : spacify(`#${team2Rank}`, 3);
+  
+  if (team1Rank > 25 && team2Rank  > 25) {
+    team1DisplayRank = "";
+    team2DisplayRank = "";
+  }
+
+  let line1Extra = "";
+  let line2Extra = "";
+  let extraWidth = 0;
+  
+  switch (status) {
+    case ESPN_GAME_STATUSES.STATUS_SCHEDULED:
+      const startDate = new Date(competition.startDate);
+      let hours = startDate.getHours();
+      const ampm = hours > 11 > 0 ? "PM" :  "AM";
+      if (hours > 11 > 0) {
+        hours %= 12;
       }
-    );
-    
-    const gameMoment = moment.tz(gameEvent.date, 'America/Chicago');
-    const gameStatusObject = gameEvent.competitions[0].status;
-    const gameStatus = gameStatusObject.type.name;
-    
-    const competitors = gameEvent.competitions[0].competitors;
-    
-    const team1 = competitors[0];
-    const team2 = competitors[1];
-    
-    if (gameStatus === ESPN_GAME_STATUSES.STATUS_SCHEDULED) {
-      const formattedGameDate = gameMoment.format('MM/DD h:mm A');
-      send(`${gameEvent.name} begins on ${formattedGameDate} CT`);
-    } else if (gameStatus === ESPN_GAME_STATUSES.STATUS_IN_PROGRESS) {
-      let message = "";
-      if (team1.score > team2.score) {
-        message = `${team1.team.location} leads ${team2.team.location}`;
-      } else {
-        message = `${team2.team.location} leads ${team1.team.location}`;  
-      }
-      message += `by a score of ${team1.team.score} to ${team2.team.score} with ${gameStatusObject.displayClock} left in the ${ordinalConverter(gameStatusObject.period)}`;
+      const minutes = startDate.getMinutes();
+      extraWidth = 13;
+      const formatedMinutes = minutes < 10 ? `0${minutes}` :  minutes;
+      const date = new Date();
       
-      send(message);
-      
-    } else if (gameStatus === ESPN_GAME_STATUSES.STATUS_FINAL) {
-      if (team1.score > team2.score) {
-        send(`${team1.team.location} defeated ${team2.team.location} by a score of ${team1.team.score} to ${team2.team.score}`);
-      } else {
-        send(`${team2.team.location} defeated ${team1.team.location} by a score of ${team2.team.score} to ${team1.team.score}`);  
+      let formattedDate = "";
+      if (date.getDate() != startDate.getDate()) {
+        formattedDate = (startDate.getMonth() + 1) + "/" + startDate.getDate() + " ";
       }
-    }
+      line1Extra = repeat(" ", extraWidth);
+      line2Extra = spacify(` ${formattedDate}${hours}:${formatedMinutes} ${ampm}`, extraWidth);
+      
+      break;  
+    case ESPN_GAME_STATUSES.STATUS_FINAL:
+      const score1 = competition.competitors[0].score;
+      const score2 = competition.competitors[1].score;
+      
+      line1Extra = spacify(score1, 3) + "|" + repeat(" ", 3);
+      line2Extra = spacify(score2, 3) + "|" + spacify("F", 3);
+      extraWidth = 7;
+      break;
+    case ESPN_GAME_STATUSES.STATUS_IN_PROGRESS:
+      const score1inProgress = competition.competitors[0].score;
+      const score2inProgress = competition.competitors[1].score;
+      const clock = competition.status.displayClock;
+      const period = ordinalConverter(competition.status.period);
+      
+      
+      line1Extra = spacify(score1inProgress, 3) + "|" + spacify(clock, 5);
+      line2Extra = spacify(score2inProgress, 3) + "|" + spacify(period, 5);
+      extraWidth = 9;
+      break;
+  }
+  const line = repeat('-', width + 1 + extraWidth + 1) + "|\n";
+  
+  return line + "|" + spacify(`${team1DisplayRank}${firstTeam}`, width) + "|" + line1Extra + "|\n" 
+  + line + "|" + spacify(`${team2DisplayRank}${secondTeam}`, width) + "|" + line2Extra + "|\n" +line;
+}
+
+const scoreHandler = (send) => {
+  const url = 'http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?groups=4';
+  
+  axios.get(url).then(({data}) => {
+    const {events: allEvents} = data;
+    
+    
+    send(allEvents.reduce((acc, score) => {
+      return acc + "```" + prettifyScoreboard(score) + "```";
+    }, ""));
+
   }).catch (e => {
+    console.error(e);
     send('Something went wrong, please try again. If this persists, please message /u/brihoang or /u/chrislabeard')
   }) ;  
 }
