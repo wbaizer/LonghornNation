0cfefe3f882c3cc43d1bbf1b159d9092582fc66c
Brian Hoang <>
2019-11-17 13:24:47 -0800

    turn off giles

--- ReplyBots/gilesHandler.js
+++ ReplyBots/gilesHandler.js
@@ -1,5 +1,6 @@
 
 const gilesHandler = (message, reply) => {
+  return;
   if (message.toLowerCase().includes('giles')) {
     reply('https://media.discordapp.net/attachments/289481914384121856/599360340144160778/gilin.gif');
   }
