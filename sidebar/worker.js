7233f5c8690bebfd8920f1569d2c8e102f836b96
reallybeard <chris@ideasbynature.com>
2021-12-12 00:17:39 -0600

    Update worker.js
    add new job to the worker

--- sidebar/worker.js
+++ sidebar/worker.js
@@ -1,27 +1,37 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const cron = require("node-cron");
-const moment = require('moment');
-const snoowrap = require('snoowrap');
-const express = require('express');
-const path = require('path');
-const TurndownService = require('turndown');
-const turndownPluginGfm = require('turndown-plugin-gfm');
+const moment = require("moment");
+const snoowrap = require("snoowrap");
+const express = require("express");
+const path = require("path");
+const TurndownService = require("turndown");
+const turndownPluginGfm = require("turndown-plugin-gfm");
 
-const { fetchTeamSchedule } = require('./utils/schedule');
-const { message, client } = require('./utils/discord');
-const { buildTitle } = require('./utils/gameThread');
-const teamLink = require('./static_data/teams.reddit.json');
-const networks = require('./static_data/networks.json');
-let agenda = require('./jobs/agenda');
+const { fetchTeamSchedule } = require("./utils/schedule");
+const { message, client } = require("./utils/discord");
+const { buildTitle } = require("./utils/gameThread");
+const teamLink = require("./static_data/teams.reddit.json");
+const networks = require("./static_data/networks.json");
+let agenda = require("./jobs/agenda");
 
-
-
-console.log('Starting Up App');
-agenda.on('ready', function() {
-    agenda.create('Update Sidebar').unique({'subreddit': process.env.SUBREDDIT}).repeatEvery('0 0 * * *', {skipImmediate: false}).save();
-    agenda.create('Free Talk Thread').unique({'ftt-sub': process.env.SUBREDDIT}).repeatEvery('0 0 * * *', {skipImmediate: true}).save();
+console.log("Starting Up App");
+agenda.on("ready", function () {
+  agenda
+    .create("Update Sidebar")
+    .unique({ subreddit: process.env.SUBREDDIT })
+    .repeatEvery("0 0 * * *", { skipImmediate: false })
+    .save();
+  agenda
+    .create("Free Talk Thread")
+    .unique({ "ftt-sub": process.env.SUBREDDIT })
+    .repeatEvery("0 0 * * *", { skipImmediate: true })
+    .save();
+  agenda
+    .create("Sports Talk Thread")
+    .unique({ "ftt-sub": process.env.SUBREDDIT })
+    .repeatEvery("0 0 * * *", { skipImmediate: true })
+    .save();
 });
 
-
-console.log('created recurring event');
+console.log("created recurring event");
