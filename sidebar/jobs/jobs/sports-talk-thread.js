4dc1346532d5ae775f5f48b3f16fcc8e65902b14
reallybeard <chris@ideasbynature.com>
2021-12-13 23:39:58 -0600

    Update sports-talk-thread.js

--- sidebar/jobs/jobs/sports-talk-thread.js
+++ sidebar/jobs/jobs/sports-talk-thread.js
@@ -1,46 +1,69 @@
-import express from 'express';
-import { join } from 'path';
-import moment from 'moment';
-import TurndownService from 'turndown';
-import { gfm as _gfm } from 'turndown-plugin-gfm';
-import { reddit, getLastThread } from '../../utils/reddit';
-
-const gfm = _gfm;
-const turndownService = new TurndownService();
+const express = require("express");
+const path = require("path");
+const moment = require("moment");
+const TurndownService = require("turndown");
+const turndownPluginGfm = require("turndown-plugin-gfm");
+const { reddit, getLastThread } = require("../../utils/reddit");
+const {
+  getWeather,
+  getRecentPosts,
+  getRecentTweets,
+} = require("../../utils/ftt");
+const { tsCalendar } = require("../../utils/service");
+const { message } = require("../../utils/discord");
+var gfm = turndownPluginGfm.gfm;
+var turndownService = new TurndownService();
 turndownService.use(gfm);
 
-export default function (agenda) {
-  agenda.define('Sports Talk Thread', function (_, done) {
-    console.log('Job Starting...');
-    const app = express();
-    app.set('views', join(__dirname, '../../views'));
-    app.set('view engine', 'ejs');
+module.exports = function (agenda) {
+  agenda.define("Sports Talk Thread", function (job, done) {
+    console.log("im running");
+    var app = express();
+    app.set("views", path.join(__dirname, "../../views"));
+    app.set("view engine", "ejs");
 
-    const date = moment(Date.now()).format('M/D/Y');
-    const longDate = moment(Date.now()).format('M/D/Y hh:mm A');
-    const day = moment(Date.now()).format("dddd['s]");
-    async function getSTT() {
-      const last_thread = await getLastThread('Sports Talk Thread');
-      const data = {
+    var date = moment(Date.now()).format("M/D/Y");
+    var date_2 = moment(Date.now()).format("M/D/Y hh:mm A");
+    var day = moment(Date.now()).format("dddd['s]");
+    async function getFTT() {
+      var weather = await getWeather();
+      var posts = await getRecentPosts(reddit, [
+        "All",
+        "CFB",
+        "LonghornNation",
+      ]);
+      var tweets = await getRecentTweets([
+        "Hcard7",
+        "CoachSark",
+        "TexasFootball",
+        "_delconte",
+      ]);
+      var last_thread = await getLastThread("Sports Talk Thread");
+      var calendar = await tsCalendar();
+      var data = {
         date: {
           short: date,
-          long: longDate,
+          long: date_2,
         },
         last_thread: last_thread || null,
+        weather: weather,
+        top: posts,
+        tweets: tweets,
+        calendar: calendar,
       };
-      app.render('sports-talk-thread', { data }, function (err, doc) {
+      app.render("sports-talk-thread", { data }, function (err, doc) {
         if (err) {
           done(err);
         }
-        const markdown = turndownService.turndown(doc, { gfm: true });
-        const post_title = '[' + date + '] ' + day + ' Sports Talk Thread';
+        var markdown = turndownService.turndown(doc, { gfm: true });
+        var post_title = "[" + date + "] " + day + " Sports Talk Thread";
         reddit
           .getSubreddit(process.env.SUBREDDIT)
           .submitSelfpost({ title: post_title, text: markdown })
           .sticky()
-          .setSuggestedSort('new')
+          .setSuggestedSort("new")
           .approve()
-          .then(() => {
+          .then((data) => {
             if (last_thread && last_thread.id) {
               reddit
                 .getSubmission(last_thread.id)
@@ -58,8 +81,9 @@ export default function (agenda) {
           .catch((err) => {
             done(err);
           });
+        //message(process.env.DISCORD_CHANNEL, false, `MoOooOoo FTT posted on ${process.env.SUBREDDIT}!`);
       });
     }
-    getSTT();
+    getFTT();
   });
-}
+};
