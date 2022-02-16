6481098458e7ee458f5ed45f5b70ed788976558b
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-02-16 16:54:15 -0600

    update b12 standings

--- sidebar/utils/service.js
+++ sidebar/utils/service.js
@@ -18,7 +18,7 @@ async function texasSports(agenda) {
         rankings: {}
     };
     var schedule = 'https://texassports.com/schedule.aspx?path=baseball';
-    var standings = 'https://big12sports.com/standings.aspx?standings=166';
+    var standings = 'https://big12sports.com/standings.aspx?standings=178';
     return axios.all([
         axios.get(standings)
     ])
